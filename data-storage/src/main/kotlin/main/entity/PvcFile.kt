f98d25df55a7b2b885381e4e42b0c75c1dc149fd
he1ex-tG <gosu.he1ex@gmail.com>
2024-01-03 15:06:46 +0500

    Add entities

---
+++ data-storage/src/main/kotlin/main/entity/PvcFile.kt
@@ -0,0 +1,24 @@
+package main.entity
+
+import main.dto.PvcFileDto
+import main.dto.PvcFileInfoDto
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+@Document(collection = "PVC_file")
+class PvcFile(
+    @Id
+    var id: String? = null,
+    val filename: String,
+    val pvcUserId: String,
+    val dateTime: LocalDateTime = OffsetDateTime.now(ZoneOffset.UTC).toLocalDateTime()
+) {
+
+    constructor(pvcFileDto: PvcFileDto, pvcUserId: String) : this(null, pvcFileDto.filename, pvcUserId)
+
+    fun toPvcFileInfoDto(): PvcFileInfoDto = PvcFileInfoDto(id ?: throw NullPointerException(), filename)
+    fun toPvcFileDto(file: ByteArray): PvcFileDto = PvcFileDto(filename, file)
+}
---
+++ data-storage/src/main/kotlin/main/entity/PvcUser.kt
@@ -0,0 +1,12 @@
+package main.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "PVC_user")
+class PvcUser(
+    @Id
+    var id: String? = null,
+    var username: String,
+    var password: String
+)
