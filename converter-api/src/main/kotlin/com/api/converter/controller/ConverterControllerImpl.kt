b05d60ba33762a3675301ea7150fb91fec225aa8
he1ex-tG <gosu.he1ex@gmail.com>
2024-01-24 00:30:58 +0500

    Add consumes, produces to convert method

--- converter-api/src/main/kotlin/com/api/converter/controller/ConverterControllerImpl.kt
+++ converter-api/src/main/kotlin/com/api/converter/controller/ConverterControllerImpl.kt
@@ -1,6 +1,7 @@
 package com.api.converter.controller
 
 import com.api.converter.service.ConverterService
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/api/v1/converter"], produces = ["application/json"])
+@RequestMapping(path = ["/api/v1/converter"])
 class ConverterControllerImpl(
     val converterService: ConverterService
 ) : ConverterController {
 
-    @PostMapping
+    @PostMapping(
+        consumes = [MediaType.APPLICATION_OCTET_STREAM_VALUE],
+        produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE]
+    )
     override fun convert(@RequestBody data: ByteArray): ResponseEntity<ByteArray> {
         return ResponseEntity
             .ok()
