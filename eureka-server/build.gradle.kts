95987358989aba7d8d7c12ea8223626e52868a00
he1ex-tG <gosu.he1ex@gmail.com>
2024-01-28 21:17:28 +0500

    Initial commit

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.processor"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
