2da13099e585749ba2bf90479f06bc236d5e82c8
he1ex-tG <gosu.he1ex@gmail.com>
2024-01-28 23:49:27 +0500

    Add eureka client configuration

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
