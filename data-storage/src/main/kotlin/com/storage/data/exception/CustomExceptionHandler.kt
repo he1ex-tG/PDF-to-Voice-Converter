c5aed236e3b9a2473bc7750e48f318f9ba9b2996
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-05 00:30:03 +0500

    Set ApiException is the part of ProblemDetails

--- data-storage/src/main/kotlin/com/storage/data/exception/CustomExceptionHandler.kt
+++ data-storage/src/main/kotlin/com/storage/data/exception/CustomExceptionHandler.kt
@@ -3,7 +3,10 @@ package com.storage.data.exception
 import com.mongodb.MongoException
 import com.objects.shared.exception.ApiException
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
@@ -14,8 +17,35 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     /**
      * ResponseEntity builder
      */
-    private fun buildResponseEntity(apiException: ApiException): ResponseEntity<ApiException> {
-        return ResponseEntity(apiException, HttpStatus.valueOf(apiException.httpStatus))
+    private fun buildResponseEntity(apiException: ApiException): ResponseEntity<Any> {
+        val errorResponse = object : ErrorResponse {
+            override fun getStatusCode(): HttpStatusCode {
+                return HttpStatus.valueOf(apiException.httpStatus)
+            }
+
+            override fun getBody(): ProblemDetail {
+                return object : ProblemDetail() {
+
+                    override fun getStatus(): Int {
+                        return apiException.httpStatus
+                    }
+
+                    override fun getTitle(): String {
+                        return HttpStatus.valueOf(apiException.httpStatus).reasonPhrase
+                    }
+
+                    override fun getDetail(): String {
+                        return ""
+                    }
+
+                    override fun getProperties(): MutableMap<String, Any> {
+                        return mutableMapOf(apiException::class.java.simpleName to apiException)
+                    }
+                }
+            }
+
+        }
+        return ResponseEntity(errorResponse.body, errorResponse.headers, errorResponse.statusCode)
     }
 
     /**
@@ -23,10 +53,14 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
      */
 
     @ExceptionHandler(MongoException::class)
-    fun handlerMongoException(ex: MongoException): ResponseEntity<ApiException> {
+    fun handlerMongoException(ex: MongoException): ResponseEntity<Any> {
         // Hide MongoDB exception messages
-        val templateEx = Throwable("Unknown database exception")
-        val e = ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), templateEx.message ?: "No message", templateEx)
+        val templateEx = Throwable("Database exception")
+        val e = ApiException(
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            message = "Data Storage module encountered an exception",
+            debugMessage = templateEx.message.orEmpty()
+        )
         return buildResponseEntity(e)
     }
 
@@ -34,14 +68,22 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
      * Custom exception handlers
      */
     @ExceptionHandler(SavePvcFileException::class)
-    fun handlerSavePvcFileException(ex: SavePvcFileException): ResponseEntity<ApiException> {
-        val e = ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.message ?: "No message", ex)
+    fun handlerSavePvcFileException(ex: SavePvcFileException): ResponseEntity<Any> {
+        val e = ApiException(
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            message = "Data Storage module encountered an exception when saving a file",
+            debugMessage = ex.message.orEmpty()
+        )
         return buildResponseEntity(e)
     }
 
     @ExceptionHandler(LoadPvcFileException::class)
-    fun handlerLoadPvcFileException(ex: LoadPvcFileException): ResponseEntity<ApiException> {
-        val e = ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.message ?: "No message", ex)
+    fun handlerLoadPvcFileException(ex: LoadPvcFileException): ResponseEntity<Any> {
+        val e = ApiException(
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            message = "Data Storage module encountered an exception when loading a file",
+            debugMessage = ex.message.orEmpty()
+        )
         return buildResponseEntity(e)
     }
 }
