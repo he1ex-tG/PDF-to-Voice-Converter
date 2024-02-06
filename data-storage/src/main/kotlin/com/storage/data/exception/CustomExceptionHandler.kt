205025ee12c4e9e6d6bacda501792a1a0f722848
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-07 00:06:14 +0500

    Add validation exception handlers

--- data-storage/src/main/kotlin/com/storage/data/exception/CustomExceptionHandler.kt
+++ data-storage/src/main/kotlin/com/storage/data/exception/CustomExceptionHandler.kt
@@ -2,13 +2,16 @@ package com.storage.data.exception
 
 import com.mongodb.MongoException
 import com.objects.shared.exception.ApiException
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
-import org.springframework.http.ProblemDetail
-import org.springframework.http.ResponseEntity
+import com.objects.shared.exception.ApiValidationException
+import jakarta.validation.ConstraintViolationException
+import jakarta.validation.ValidationException
+import org.springframework.http.*
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
@@ -51,7 +54,6 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     /**
      * Default exception handlers
      */
-
     @ExceptionHandler(MongoException::class)
     fun handlerMongoException(ex: MongoException): ResponseEntity<Any> {
         // Hide MongoDB exception messages
@@ -60,6 +62,68 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
             httpStatus = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             message = "Data Storage module encountered an exception",
             debugMessage = templateEx.message.orEmpty()
+        )
+        return buildResponseEntity(e)
+    }
+
+    @ExceptionHandler(ValidationException::class)
+    fun handleValidationException(ex: ValidationException): ResponseEntity<Any> {
+        val e = ApiException(
+            httpStatus = HttpStatus.BAD_REQUEST.value(),
+            message = "Data Storage received incorrect input data",
+            debugMessage = ""
+        )
+        return buildResponseEntity(e)
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(ex: ConstraintViolationException): ResponseEntity<Any> {
+        val e = ApiException(
+            httpStatus = HttpStatus.BAD_REQUEST.value(),
+            message = "Data Storage received incorrect input data",
+            debugMessage = ""
+        )
+        for (constraintViolation in ex.constraintViolations) {
+            val apiValidationException = ApiValidationException(
+                constraintViolation.rootBeanClass.name,
+                constraintViolation.message,
+                constraintViolation.propertyPath.toString(),
+                constraintViolation.invalidValue
+            )
+            e.subErrors.add(apiValidationException)
+        }
+        return buildResponseEntity(e)
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val e = ApiException(
+            httpStatus = HttpStatus.BAD_REQUEST.value(),
+            message = "Data Storage received incorrect input data",
+            debugMessage = "",
+        )
+        for (er in ex.bindingResult.fieldErrors) {
+            e.subErrors.add(
+                ApiValidationException(er.objectName, er.defaultMessage.orEmpty(), er.field, er.rejectedValue)
+            )
+        }
+        return buildResponseEntity(e)
+    }
+
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val e = ApiException(
+            httpStatus = HttpStatus.BAD_REQUEST.value(),
+            message = "Data Storage received incorrect input data",
+            debugMessage = ex.message.orEmpty()
         )
         return buildResponseEntity(e)
     }
