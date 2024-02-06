70c743fad7a79aaf8372bc774f8c018772014a5a
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-06 22:56:29 +0500

    Add validation dependence

--- data-storage/build.gradle.kts
+++ data-storage/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
