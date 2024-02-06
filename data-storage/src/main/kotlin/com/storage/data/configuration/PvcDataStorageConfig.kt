b01659a8e55ee821e85fbc1ab877b0564ff18260
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-07 00:05:23 +0500

    Fix configuration properties prefix

--- data-storage/src/main/kotlin/com/storage/data/configuration/PvcDataStorageConfig.kt
+++ data-storage/src/main/kotlin/com/storage/data/configuration/PvcDataStorageConfig.kt
@@ -2,7 +2,7 @@ package com.storage.data.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "pvc.dataStorage")
+@ConfigurationProperties(prefix = "pvc.data-storage")
 class PvcDataStorageConfig {
     lateinit var localStoragePath: String
 }
