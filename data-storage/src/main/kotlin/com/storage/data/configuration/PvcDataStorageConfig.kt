ff8d1b952d079e78aa2f7dc24678f72bf8f8e98f
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-13 00:59:09 +0500

    Add storage size control

--- data-storage/src/main/kotlin/com/storage/data/configuration/PvcDataStorageConfig.kt
+++ data-storage/src/main/kotlin/com/storage/data/configuration/PvcDataStorageConfig.kt
@@ -5,4 +5,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "pvc.data-storage")
 class PvcDataStorageConfig {
     lateinit var localStoragePath: String
+    lateinit var localStorageSize: String
 }
