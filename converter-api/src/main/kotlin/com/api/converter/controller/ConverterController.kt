2d3b4ebb1bb521e40f9da5d18f9913ab07706614
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-13 23:48:57 +0500

    Remove unnecessary annotation

--- converter-api/src/main/kotlin/com/api/converter/controller/ConverterController.kt
+++ converter-api/src/main/kotlin/com/api/converter/controller/ConverterController.kt
@@ -1,9 +1,7 @@
 package com.api.converter.controller
 
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 
-@Validated
 interface ConverterController {
 
     fun convert(
