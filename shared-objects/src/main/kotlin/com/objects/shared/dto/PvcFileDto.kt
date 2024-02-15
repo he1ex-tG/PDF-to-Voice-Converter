0886e17918065f902b48ff6f6caaae3d7e6d13f8
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-15 22:57:24 +0500

    Delete an inherited class, set annotations on the base class in the shared objects module

--- shared-objects/src/main/kotlin/com/objects/shared/dto/PvcFileDto.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/dto/PvcFileDto.kt
@@ -1,6 +1,10 @@
 package com.objects.shared.dto
 
+import jakarta.validation.constraints.NotEmpty
+
 open class PvcFileDto(
+    @field:NotEmpty(message = "{dataStorage.pvcFileConstrainedDto.filename.isEmpty}")
     open val filename: String,
+    @field:NotEmpty(message = "{dataStorage.pvcFileConstrainedDto.file.isEmpty}")
     open val file: ByteArray
 )
