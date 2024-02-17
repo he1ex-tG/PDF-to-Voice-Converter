87abd759221ebc74a4237f6a0dfc7ca212a1adbb
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-17 23:31:19 +0500

    Remove unnecessary annotations from overridden methods

--- converter-api/src/main/kotlin/com/api/converter/controller/ConverterApiMainControllerImpl.kt
+++ converter-api/src/main/kotlin/com/api/converter/controller/ConverterApiMainControllerImpl.kt
@@ -3,7 +3,6 @@ package com.api.converter.controller
 import com.api.converter.service.ConverterService
 import com.objects.shared.controller.ConverterApiMainController
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -15,5 +14,5 @@ class ConverterApiMainControllerImpl(
 ) : ConverterApiMainController {
 
     @ResponseStatus(value = HttpStatus.OK)
-    override fun convert(@RequestBody data: ByteArray): ByteArray = converterService.convert(data)
+    override fun convert(data: ByteArray): ByteArray = converterService.convert(data)
 }
--- data-storage/src/main/kotlin/com/storage/data/controller/DataStorageMainControllerImpl.kt
+++ data-storage/src/main/kotlin/com/storage/data/controller/DataStorageMainControllerImpl.kt
@@ -5,8 +5,6 @@ import com.objects.shared.dto.PvcFileDto
 import com.objects.shared.dto.PvcFileInfoDto
 import com.storage.data.service.PvcFileService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -18,11 +16,11 @@ class DataStorageMainControllerImpl(
 ) : DataStorageMainController {
 
     @ResponseStatus(value = HttpStatus.OK)
-    override fun downloadPvcFile(@PathVariable("id") id: String): PvcFileDto = pvcFileService.loadPvcFile(id)
+    override fun downloadPvcFile(id: String): PvcFileDto = pvcFileService.loadPvcFile(id)
 
     @ResponseStatus(value = HttpStatus.OK)
     override fun downloadPvcFileList(): List<PvcFileInfoDto> = pvcFileService.getPvcFileList()
 
     @ResponseStatus(value = HttpStatus.CREATED)
-    override fun uploadPvcFile(@RequestBody pvcFile: PvcFileDto): PvcFileInfoDto = pvcFileService.savePvcFile(pvcFile)
+    override fun uploadPvcFile(pvcFile: PvcFileDto): PvcFileInfoDto = pvcFileService.savePvcFile(pvcFile)
 }
