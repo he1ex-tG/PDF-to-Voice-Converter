c79c02a8263217f67e844d07ca972ca2f1f16184
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-24 15:05:22 +0500

    Fix DataStorageNotAvailable handler

--- processor/src/main/kotlin/com/processor/exception/CustomExceptionHandler.kt
+++ processor/src/main/kotlin/com/processor/exception/CustomExceptionHandler.kt
@@ -1,8 +1,6 @@
 package com.processor.exception
 
-import com.objects.shared.exception.ApiException
 import org.springframework.http.*
-import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -14,36 +12,6 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     /**
      * ResponseEntity builder
      */
-    private fun buildResponseEntity(apiException: ApiException): ResponseEntity<Any> {
-        val errorResponse = object : ErrorResponse {
-            override fun getStatusCode(): HttpStatusCode {
-                return HttpStatus.valueOf(apiException.httpStatus)
-            }
-
-            override fun getBody(): ProblemDetail {
-                return object : ProblemDetail() {
-
-                    override fun getStatus(): Int {
-                        return apiException.httpStatus
-                    }
-
-                    override fun getTitle(): String {
-                        return HttpStatus.valueOf(apiException.httpStatus).reasonPhrase
-                    }
-
-                    override fun getDetail(): String {
-                        return ""
-                    }
-
-                    override fun getProperties(): MutableMap<String, Any> {
-                        return mutableMapOf(ApiException::class.java.simpleName to apiException)
-                    }
-                }
-            }
-
-        }
-        return ResponseEntity(errorResponse.body, errorResponse.headers, errorResponse.statusCode)
-    }
 
     /**
      * Default exception handlers
@@ -82,13 +50,11 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
     }
 
     @ExceptionHandler(DataStorageNotAvailable::class)
-    fun handlerDataStorageNotAvailable(ex: DataStorageNotAvailable): ResponseEntity<Any> {
-        val e = ApiException(
-            httpStatus = HttpStatus.SERVICE_UNAVAILABLE.value(),
-            message = "Data Storage module does not respond to requests",
-            debugMessage = ex.message.orEmpty()
-        )
-        return buildResponseEntity(e)
+    fun handlerDataStorageNotAvailable(ex: DataStorageNotAvailable, request: WebRequest): ResponseEntity<Any>? {
+        val status = HttpStatus.SERVICE_UNAVAILABLE
+        val defaultDetail = "Data Storage module does not respond to requests"
+        val problem = createProblemDetail(ex, status, defaultDetail, null, null, request)
+        val headers = HttpHeaders()
+        return handleExceptionInternal(ex, problem, headers, status, request)
     }
-
 }
