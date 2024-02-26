37381943d02902a5a93bc1f1dbf30739bcac98ca
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-26 23:37:02 +0500

    Override toString, equals, hashCode

--- data-storage/src/main/kotlin/com/storage/data/entity/PvcFile.kt
+++ data-storage/src/main/kotlin/com/storage/data/entity/PvcFile.kt
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
 
 @Document(collection = "PVC_file")
 class PvcFile(
@@ -14,11 +15,27 @@ class PvcFile(
     var id: String? = null,
     val filename: String,
     val pvcUserId: String,
-    val dateTime: LocalDateTime = OffsetDateTime.now(ZoneOffset.UTC).toLocalDateTime()
+    val dateTime: LocalDateTime = OffsetDateTime.now(ZoneOffset.UTC).toLocalDateTime().truncatedTo(ChronoUnit.SECONDS)
 ) {
 
     constructor(pvcFileDto: PvcFileDto, pvcUserId: String) : this(null, pvcFileDto.filename, pvcUserId)
 
     fun toPvcFileInfoDto(): PvcFileInfoDto = PvcFileInfoDto(id ?: throw NullPointerException(), filename)
     fun toPvcFileDto(file: ByteArray): PvcFileDto = PvcFileDto(filename, file)
+
+    override fun toString(): String {
+        return "PvcFile instance: id = ${id ?: "null"}, filename = $filename, " +
+                "pvcUserId = $pvcUserId, dateTime = $dateTime"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PvcFile) return false
+        if ((id == other.id) && (filename == other.filename) && (pvcUserId == other.pvcUserId) && (dateTime == other.dateTime)) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * filename.hashCode() + 31 * pvcUserId.hashCode() + 31 * dateTime.hashCode()
+    }
 }
