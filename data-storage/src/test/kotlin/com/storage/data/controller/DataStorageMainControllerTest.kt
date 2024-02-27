7c4d63a0e1f78c632032b74726f0ec36e2e6971a
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-27 23:23:41 +0500

    Add downloadPvcFile, downloadPvcFileList WebMvcTest

---
+++ data-storage/src/test/kotlin/com/storage/data/controller/DataStorageMainControllerTest.kt
@@ -0,0 +1,182 @@
+package com.storage.data.controller
+
+import com.mongodb.MongoException
+import com.objects.shared.dto.PvcFileDto
+import com.objects.shared.dto.PvcFileInfoDto
+import com.storage.data.exception.LoadPvcFileException
+import com.storage.data.service.PvcFileService
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.*
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest(controllers = [DataStorageMainControllerImpl::class])
+class ConverterApiMainControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var pvcFileService: PvcFileService
+
+    @Test
+    fun `downloadPvcFile gets null in path - 404`() {
+        mockMvc.get("/api/v1/files/") {
+            content = null
+        }.andExpect {
+            status {
+                isNotFound()
+            }
+            content {
+                jsonPath("type") {
+                    value("about:blank")
+                }
+                jsonPath("title") {
+                    value("Not Found")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `downloadPvcFile gets id but database error - throw mongo base exception`() {
+        given(pvcFileService.loadPvcFile(MockitoHelper.anyObject())).willAnswer {
+            throw MongoException("Some exception")
+        }
+        mockMvc.get("/api/v1/files/fileRandomId") {
+            content = null
+        }.andExpect {
+            status {
+                isInternalServerError()
+            }
+            content {
+                jsonPath("type") {
+                    value("about:blank")
+                }
+                jsonPath("title") {
+                    value("Internal Server Error")
+                }
+                jsonPath("detail") {
+                    value(Matchers.containsString("Database exception"))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `downloadPvcFile gets id but file not read or absent - throw LoadPvcFileException exception`() {
+        given(pvcFileService.loadPvcFile(MockitoHelper.anyObject())).willAnswer {
+            throw LoadPvcFileException()
+        }
+        mockMvc.get("/api/v1/files/fileRandomId") {
+            content = null
+        }.andExpect {
+            status {
+                isInternalServerError()
+            }
+            content {
+                jsonPath("type") {
+                    value("about:blank")
+                }
+                jsonPath("title") {
+                    value("Internal Server Error")
+                }
+                jsonPath("detail") {
+                    value(Matchers.containsString("Load file function thrown an exception, file not load"))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `downloadPvcFile gets id and ends with normal - 200`() {
+        given(pvcFileService.loadPvcFile(MockitoHelper.anyObject())).willAnswer {
+            PvcFileDto("name", byteArrayOf(1, 2, 3))
+        }
+        mockMvc.get("/api/v1/files/fileRandomId") {
+            content = null
+        }.andExpect {
+            status {
+                isOk()
+            }
+            content {
+                jsonPath("filename") {
+                    value("name")
+                }
+                jsonPath("file") {
+                    isNotEmpty()
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `downloadPvcFileList executed but database error - throw mongo base exception`() {
+        given(pvcFileService.getPvcFileList()).willAnswer {
+            throw MongoException("Some exception")
+        }
+        mockMvc.get("/api/v1/files") {
+            content = null
+        }.andExpect {
+            status {
+                isInternalServerError()
+            }
+            content {
+                jsonPath("type") {
+                    value("about:blank")
+                }
+                jsonPath("title") {
+                    value("Internal Server Error")
+                }
+                jsonPath("detail") {
+                    value(Matchers.containsString("Database exception"))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `downloadPvcFileList executed and ends with normal - 200`() {
+        given(pvcFileService.getPvcFileList()).willAnswer {
+            listOf(
+                PvcFileInfoDto("id1", "filename1"),
+                PvcFileInfoDto("id2", "filename2")
+            )
+        }
+        mockMvc.get("/api/v1/files") {
+            content = null
+        }.andExpect {
+            status {
+                isOk()
+            }
+            content {
+                jsonPath("$.[0].id") {
+                    value("id1")
+                }
+                jsonPath("$.[0].filename") {
+                    value("filename1")
+                }
+                jsonPath("$.[1].id") {
+                    value("id2")
+                }
+                jsonPath("$.[1].filename") {
+                    value("filename2")
+                }
+            }
+        }
+    }
+}
+
+object MockitoHelper {
+    fun <T> anyObject(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+    @Suppress("UNCHECKED_CAST")
+    fun <T> uninitialized(): T = null as T
+}
