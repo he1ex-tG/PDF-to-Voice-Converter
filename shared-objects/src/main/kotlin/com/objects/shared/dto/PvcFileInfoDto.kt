debe7ce9e5859bbc15aaf30d5b2c249da1fc5cf7
he1ex-tG <gosu.he1ex@gmail.com>
2024-02-28 23:31:12 +0500

    Override toString, equals, hashCode functions

--- shared-objects/src/main/kotlin/com/objects/shared/dto/PvcFileInfoDto.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/dto/PvcFileInfoDto.kt
@@ -3,4 +3,20 @@ package com.objects.shared.dto
 open class PvcFileInfoDto(
     open val id: String,
     open val filename: String
-)
+) {
+
+    override fun toString(): String {
+        return "PvcFileInfoDto instance: id = $id, filename = $filename"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PvcFileInfoDto) return false
+        if ((id == other.id) && (filename == other.filename)) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * id.hashCode() + 31 * filename.hashCode()
+    }
+}
