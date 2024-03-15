58b77bcb8fb25858def924084e5c8e15f3e6ddd1
User <he1ex-tg@yandex.ru>
2024-03-15 15:26:36 +0500

    Handle ProcessorException in ControllerAdvice

--- user-interface/src/main/kotlin/com/iface/user/exception/CustomExceptionHandler.kt
+++ user-interface/src/main/kotlin/com/iface/user/exception/CustomExceptionHandler.kt
@@ -1,15 +1,15 @@
 package com.iface.user.exception
 
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.servlet.ModelAndView
 
 @ControllerAdvice
-class CustomExceptionHandler : ResponseEntityExceptionHandler() {
+class CustomExceptionHandler {
+
+    /**
+     * ResponseEntity builder
+     */
 
     /**
      * Default exception handlers
@@ -19,11 +19,11 @@ class CustomExceptionHandler : ResponseEntityExceptionHandler() {
      * Custom exception handlers
      */
     @ExceptionHandler(ProcessorException::class)
-    fun handlerProcessorException(ex: ProcessorException, request: WebRequest): ResponseEntity<Any>? {
-        val status = HttpStatus.INTERNAL_SERVER_ERROR
-        val defaultDetail = ex.localizedMessage
-        val problem = createProblemDetail(ex, status, defaultDetail, null, null, request)
-        val headers = HttpHeaders()
-        return handleExceptionInternal(ex, problem, headers, status, request)
+    fun handlerProcessorException(ex: ProcessorException): ModelAndView {
+        return ModelAndView().apply {
+            viewName = "error"
+            addObject("status", ex.status ?: "No status")
+            addObject("message", ex.message ?: "No message")
+        }
     }
 }
