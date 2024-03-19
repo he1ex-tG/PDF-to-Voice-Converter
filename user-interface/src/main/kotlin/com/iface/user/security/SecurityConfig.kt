589c44d88364e1f00ef07a97299cf85209525431
he1ex-tG <gosu.he1ex@gmail.com>
2024-03-19 23:53:59 +0500

    Add security config

---
+++ user-interface/src/main/kotlin/com/iface/user/security/SecurityConfig.kt
@@ -0,0 +1,27 @@
+package com.iface.user.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeRequests {
+                authorize(HttpMethod.GET, "/**", hasAuthority("SCOPE_user"))
+                authorize(HttpMethod.POST, "/**", hasAuthority("SCOPE_user"))
+            }
+            oauth2Login {
+            }
+        }
+        return http.build()
+    }
+}
