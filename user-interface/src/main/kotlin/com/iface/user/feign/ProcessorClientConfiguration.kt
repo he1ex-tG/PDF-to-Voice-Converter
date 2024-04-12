1c01ab997fd3153ab9f17d579529e5a9bc5aafec
User <he1ex-tg@yandex.ru>
2024-04-12 22:46:09 +0500

    Replace hardcoded oauth2 client registration id

--- user-interface/src/main/kotlin/com/iface/user/feign/ProcessorClientConfiguration.kt
+++ user-interface/src/main/kotlin/com/iface/user/feign/ProcessorClientConfiguration.kt
@@ -2,12 +2,16 @@ package com.iface.user.feign
 
 import com.iface.user.exception.ProcessorException
 import feign.codec.ErrorDecoder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.security.OAuth2AccessTokenInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 
 class ProcessorClientConfiguration {
+
+    @Value("\${spring.application.name}")
+    lateinit var appName: String
 
     @Bean
     fun customErrorDecoder(): ErrorDecoder {
@@ -25,6 +29,6 @@ class ProcessorClientConfiguration {
 
     @Bean
     fun getOAuth2AccessTokenInterceptor(oAuth2AuthorizedClientManager: OAuth2AuthorizedClientManager): OAuth2AccessTokenInterceptor {
-        return OAuth2AccessTokenInterceptor("github", oAuth2AuthorizedClientManager)
+        return OAuth2AccessTokenInterceptor(appName, oAuth2AuthorizedClientManager)
     }
 }
