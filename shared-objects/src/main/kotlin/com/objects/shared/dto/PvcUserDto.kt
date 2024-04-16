f2b5f85f7191427481a61636d72bfc36b85804ca
User <he1ex-tg@yandex.ru>
2024-04-16 18:33:13 +0500

    Add PvcUserDto and it's validation error messages

---
+++ shared-objects/src/main/kotlin/com/objects/shared/dto/PvcUserDto.kt
@@ -0,0 +1,10 @@
+package com.objects.shared.dto
+
+import jakarta.validation.constraints.NotEmpty
+
+open class PvcUserDto(
+    @field:NotEmpty(message = "{dataStorage.pvcUserConstrainedDto.username.isEmpty}")
+    open val username: String,
+    @field:NotEmpty(message = "{dataStorage.pvcUserConstrainedDto.password.isEmpty}")
+    open val password: String
+)
