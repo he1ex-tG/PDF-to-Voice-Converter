dd6841a6860f003c31d419b32a88e131b5e230c3
User <he1ex-tg@yandex.ru>
2024-04-16 18:40:10 +0500

    Class changes: add unique field, constructor, toDTO method, override default methods

--- data-storage/src/main/kotlin/com/storage/data/entity/PvcUser.kt
+++ data-storage/src/main/kotlin/com/storage/data/entity/PvcUser.kt
@@ -1,12 +1,36 @@
 package com.storage.data.entity
 
+import com.objects.shared.dto.PvcUserDto
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "PVC_user")
 class PvcUser(
     @Id
     var id: String? = null,
-    var username: String,
-    var password: String
-)
+    @Indexed(unique = true)
+    val username: String,
+    val password: String
+) {
+
+    constructor(pvcUserDto: PvcUserDto) : this(null, pvcUserDto.username, pvcUserDto.password)
+
+    fun toPvcUserDto(): PvcUserDto = PvcUserDto(username, password)
+
+    override fun toString(): String {
+        return "PvcUser instance: id = ${id ?: "null"}, username = $username, " +
+                "password = $password"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PvcUser) return false
+        if ((id == other.id) && (username == other.username) && (password == other.password)) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * username.hashCode() + 31 * password.hashCode()
+    }
+}
