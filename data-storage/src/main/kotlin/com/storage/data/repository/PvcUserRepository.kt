8842eb30dc887b666ec2b82a2402d6fdf225bfae
User <he1ex-tg@yandex.ru>
2024-04-16 18:35:58 +0500

    Add custom findByUsername method

--- data-storage/src/main/kotlin/com/storage/data/repository/PvcUserRepository.kt
+++ data-storage/src/main/kotlin/com/storage/data/repository/PvcUserRepository.kt
@@ -2,5 +2,9 @@ package com.storage.data.repository
 
 import com.storage.data.entity.PvcUser
 import org.springframework.data.repository.CrudRepository
+import java.util.Optional
 
-interface PvcUserRepository : CrudRepository<PvcUser, String>
+interface PvcUserRepository : CrudRepository<PvcUser, String> {
+
+    fun findByUsername(username: String): Optional<PvcUser>
+}
