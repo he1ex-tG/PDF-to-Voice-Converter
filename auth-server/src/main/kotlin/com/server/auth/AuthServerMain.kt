25575868de8cd856d6a2c4f0b409e7f62e24a6af
User <he1ex-tg@yandex.ru>
2024-04-19 15:04:54 +0500

    Enable Feign clients

--- auth-server/src/main/kotlin/com/server/auth/AuthServerMain.kt
+++ auth-server/src/main/kotlin/com/server/auth/AuthServerMain.kt
@@ -2,8 +2,10 @@ package com.server.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class AuthServerMain
 
 fun main(array: Array<String>) {
