a446edb7c86342faf5404af823601fde6d4098a5
User <he1ex-tg@yandex.ru>
2024-04-20 15:46:10 +0500

    Allow access to /error

--- auth-server/src/main/kotlin/com/server/auth/security/SecurityConfig.kt
+++ auth-server/src/main/kotlin/com/server/auth/security/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.server.auth.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -44,6 +45,7 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeRequests {
+                authorize(HttpMethod.GET,"/error", permitAll)
                 authorize("/login", permitAll)
                 authorize("/registration", permitAll)
                 authorize(anyRequest, authenticated)
