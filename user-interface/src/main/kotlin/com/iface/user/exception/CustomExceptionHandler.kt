60986785c568dbcbc78cec5aabe4cdf0ce1b5859
User <he1ex-tg@yandex.ru>
2024-04-21 15:47:05 +0500

    Add issuer uri as model parameter

--- user-interface/src/main/kotlin/com/iface/user/exception/CustomExceptionHandler.kt
+++ user-interface/src/main/kotlin/com/iface/user/exception/CustomExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.iface.user.exception
 
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.ServletWebRequest
 import org.springframework.web.servlet.ModelAndView
 
 @ControllerAdvice
@@ -19,11 +20,13 @@ class CustomExceptionHandler {
      * Custom exception handlers
      */
     @ExceptionHandler(ProcessorException::class)
-    fun handlerProcessorException(ex: ProcessorException): ModelAndView {
+    fun handlerProcessorException(ex: ProcessorException, request: ServletWebRequest): ModelAndView {
+        val issuerUri = request.request.requestURI
         return ModelAndView().apply {
             viewName = "error"
             addObject("status", ex.status ?: "No status")
             addObject("message", ex.message ?: "No message")
+            addObject("issuerUri", issuerUri ?: "/")
         }
     }
 }
