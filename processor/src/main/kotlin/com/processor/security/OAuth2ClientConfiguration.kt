97e548d9d77f3d7dac12782b54f780c60bf1596f
User <he1ex-tg@yandex.ru>
2024-07-12 14:32:08 +0500

    Add security configuration

---
+++ processor/src/main/kotlin/com/processor/security/OAuth2ClientConfiguration.kt
@@ -0,0 +1,53 @@
+package com.processor.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+
+@Configuration
+class OAuth2ClientConfiguration {
+
+    @Value("\${pvc.processor.oauth2Client.clientId}")
+    lateinit var clientId: String
+    @Value("\${pvc.processor.oauth2Client.clientSecret}")
+    lateinit var clientSecret: String
+    @Value("\${spring.application.name}")
+    lateinit var appName: String
+    @Value("\${pvc.authServer.address}")
+    lateinit var authAddress: String
+    @Value("\${pvc.authServer.port}")
+    lateinit var authPort: String
+
+    @Bean
+    fun clientRegistrationRepository(): ClientRegistrationRepository {
+        return InMemoryClientRegistrationRepository(pvcUserClientRegistration())
+    }
+
+    private fun pvcUserClientRegistration(): ClientRegistration {
+        return ClientRegistration.withRegistrationId(appName)
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .redirectUri("http://localhost:7015/login/oauth2/code/auth-client")
+            .clientName("PVC processor client")
+            .scope(
+                "files:read",
+                "files:write",
+                OidcScopes.OPENID
+            )
+            .authorizationUri("$authAddress:$authPort/oauth2/v1/authorize")
+            .tokenUri("$authAddress:$authPort/oauth2/v1/token")
+            .userInfoUri("$authAddress:$authPort/connect/v1/userinfo")
+            .jwkSetUri("$authAddress:$authPort/oauth2/v1/jwks")
+            .userNameAttributeName(IdTokenClaimNames.SUB)
+            .build()
+    }
+}
---
+++ processor/src/main/kotlin/com/processor/security/SecurityConfig.kt
@@ -0,0 +1,68 @@
+package com.processor.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.RegexRequestMatcher
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Value("\${pvc.authServer.address}")
+    lateinit var authAddress: String
+    @Value("\${pvc.authServer.port}")
+    lateinit var authPort: String
+
+    @Bean
+    @Order(1)
+    fun oauth2FilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            oauth2ResourceServer {
+                jwt {
+                    jwkSetUri = "$authAddress:$authPort/oauth2/v1/jwks"
+                }
+            }
+            oauth2Client {  }
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeRequests {
+                /**
+                 * Files GET requests. Url like:
+                 *  http://localhost:7015/api/v1/files
+                 *  http://localhost:7015/api/v1/files/zzfilezz11337htg755zz
+                 */
+                authorize(
+                    RegexRequestMatcher.regexMatcher(
+                        HttpMethod.GET, "^/api/v[0-9]+/files(/[0-9a-z]+)?$"
+                    ),
+                    hasAuthority("SCOPE_user:read")
+                )
+                /**
+                 * Files POST requests. Url like:
+                 *  http://localhost:7015/api/v1/files
+                 */
+                authorize(
+                    RegexRequestMatcher.regexMatcher(
+                        HttpMethod.POST, "^/api/v[0-9]+/files$"
+                    ),
+                    hasAuthority("SCOPE_user:write")
+                )
+                authorize(anyRequest, denyAll)
+            }
+        }
+        return http.build()
+    }
+}
