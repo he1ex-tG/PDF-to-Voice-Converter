4fa413a19a8542a4a2a6bfeb82e777655b9ae25c
he1ex-tG <gosu.he1ex@gmail.com>
2024-08-05 21:52:03 +0500

    Change client registration: now auth server endpoints gets from issuer

--- user-interface/src/main/kotlin/com/iface/user/security/OAuth2ClientConfiguration.kt
+++ user-interface/src/main/kotlin/com/iface/user/security/OAuth2ClientConfiguration.kt
@@ -2,16 +2,15 @@ package com.iface.user.security
 
 import com.objects.shared.configuration.PvcConfiguration
 import com.objects.shared.security.PvcScopes
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.ClientRegistrations
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
 import org.springframework.security.oauth2.core.oidc.OidcScopes
 
 @Configuration
@@ -19,9 +18,6 @@ import org.springframework.security.oauth2.core.oidc.OidcScopes
 class OAuth2ClientConfiguration(
     private val pvcConfiguration: PvcConfiguration
 ) {
-
-    @Value("\${spring.application.name}")
-    lateinit var appName: String
 
     @Bean
     fun clientRegistrationRepository(): ClientRegistrationRepository {
@@ -33,23 +29,19 @@ class OAuth2ClientConfiguration(
     }
 
     private fun pvcUserClientRegistration(): ClientRegistration {
-        return ClientRegistration.withRegistrationId(appName)
+        return ClientRegistrations.fromOidcIssuerLocation("${pvcConfiguration.authServer.address}:${pvcConfiguration.authServer.port}")
+            .registrationId(pvcConfiguration.userInterface.appName)
             .clientId(pvcConfiguration.userInterface.oAuth2Client.clientId)
             .clientSecret(pvcConfiguration.userInterface.oAuth2Client.clientSecret)
             .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
             .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-            .redirectUri("${pvcConfiguration.userInterface.address}:${pvcConfiguration.userInterface.port}/login/oauth2/code/user-client")
+            .redirectUri("${pvcConfiguration.userInterface.address}:${pvcConfiguration.userInterface.port}/login/oauth2/code/${pvcConfiguration.userInterface.appName}")
             .clientName("PVC UI user client")
             .scope(
                 PvcScopes.USER.READ,
                 PvcScopes.USER.WRITE,
                 OidcScopes.OPENID
             )
-            .authorizationUri("${pvcConfiguration.authServer.address}:${pvcConfiguration.authServer.port}/oauth2/v1/authorize")
-            .tokenUri("${pvcConfiguration.authServer.address}:${pvcConfiguration.authServer.port}/oauth2/v1/token")
-            .userInfoUri("${pvcConfiguration.authServer.address}:${pvcConfiguration.authServer.port}/connect/v1/userinfo")
-            .jwkSetUri("${pvcConfiguration.authServer.address}:${pvcConfiguration.authServer.port}/oauth2/v1/jwks")
-            .userNameAttributeName(IdTokenClaimNames.SUB)
             .build()
     }
 }
