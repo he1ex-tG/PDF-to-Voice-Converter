49600ff2d2f10e01b91173cfa2156b19fe5f8948
he1ex-tG <gosu.he1ex@gmail.com>
2024-08-05 21:52:37 +0500

    Add appName to the models

--- shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldAuthServer.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldAuthServer.kt
@@ -3,5 +3,6 @@ package com.objects.shared.configuration
 class FieldAuthServer(
     val oAuth2Client: SubfieldOAuth2Client,
     val address: String,
-    val port: String
+    val port: String,
+    val appName: String
 )
--- shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldProcessor.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldProcessor.kt
@@ -3,5 +3,6 @@ package com.objects.shared.configuration
 class FieldProcessor(
     val oAuth2Client: SubfieldOAuth2Client,
     val address: String,
-    val port: String
+    val port: String,
+    val appName: String
 )
