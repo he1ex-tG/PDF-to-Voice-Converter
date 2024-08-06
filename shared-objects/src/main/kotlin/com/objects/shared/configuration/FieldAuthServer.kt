c1bf8bc6bd468b1dd55bd022e5c1bfa479a33085
he1ex-tG <gosu.he1ex@gmail.com>
2024-08-06 09:53:30 +0500

    Restructure configuration files

--- shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldAuthServer.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldAuthServer.kt
@@ -2,7 +2,7 @@ package com.objects.shared.configuration
 
 class FieldAuthServer(
     val oAuth2Client: SubfieldOAuth2Client,
-    val address: String,
+    val uri: String,
     val port: String,
     val appName: String
 )
--- shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldProcessor.kt
+++ shared-objects/src/main/kotlin/com/objects/shared/configuration/FieldProcessor.kt
@@ -2,7 +2,7 @@ package com.objects.shared.configuration
 
 class FieldProcessor(
     val oAuth2Client: SubfieldOAuth2Client,
-    val address: String,
+    val uri: String,
     val port: String,
     val appName: String
 )
